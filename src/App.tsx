753ea7ebdd066c01897b4daf59bf72fb5b498650
Anthonin <Anthonincolaspro@gmail.com>
2022-06-19 00:25:02 +0200

    [CHANGES]: eslint configuration change + home page modification

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BsFillSuitHeartFill } from 'react-icons/bs';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Template Typescript using esLint and Prettier Made with {<BsFillSuitHeartFill color='red' />} by{' '}
+          <a href='https://www.github.com/st3rbenn' style={{ textDecoration: 'none' }}>
+            St3rbenn
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
